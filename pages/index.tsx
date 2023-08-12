d6399151a0ae2d9798df0849a169325024cb1765
sukpo61 <86722178+sukpo61@users.noreply.github.com>
2023-08-13 00:32:05 +0900

    feature : auth, toast, layout setting

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,27 +1,4 @@
-import type { NextPage } from "next";
-import PartyList from "../components/common/partyList";
-
-export default function Home() {
-  return (
-    <div>
-      {partyList.map((e, i) => {
-        return <PartyList title={e.title} id={e.id} key={i} />;
-      })}
-    </div>
-  );
-}
-
-const partyList = [
-  {
-    title: "1",
-    id: "1",
-  },
-  {
-    title: "2",
-    id: "2",
-  },
-  {
-    title: "3",
-    id: "3",
-  },
-];
+const Home = () => {
+  return <div>홈</div>;
+};
+export default Home;
