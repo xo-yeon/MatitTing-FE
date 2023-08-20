1c1e524603763f58a78aaa842026998e7ff0d571
minsuKang <minsu910725@gmail.com>
2023-08-20 15:09:02 +0900

    Fix: ts 에러 수정 및 import 경로 수정

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,20 +1,22 @@
-import NextAuth, { NextAuthOptions } from "next-auth";
+import NextAuth, { NextAuthOptions } from 'next-auth';
 
-import KakaoProvider from "next-auth/providers/kakao";
+import KakaoProvider from 'next-auth/providers/kakao';
 
 export const authOptions: NextAuthOptions = {
   secret: process.env.SECRET,
   providers: [
     KakaoProvider({
-      clientId: process.env.KAKAO_CLIENT_ID,
-      clientSecret: process.env.KAKAO_CLIENT_SECRET,
+      clientId: process.env.KAKAO_CLIENT_ID || '',
+      clientSecret: process.env.KAKAO_CLIENT_SECRET || '',
     }),
   ],
   callbacks: {
     async signIn({ user }): Promise<boolean | string> {
       try {
         return true;
-      } catch (error: any) {}
+      } catch (error: any) {
+        return error.message;
+      }
     },
   },
 };
