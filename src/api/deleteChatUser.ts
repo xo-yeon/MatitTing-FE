0c51699b9bc1d7545deb6507611e7fefc0aac974
xo-yeon <27xo.yeon@gmail.com>
2024-05-03 14:22:56 +0900

    파티 유저 강퇴 api 추가

--- src/api/deleteChatUser.ts
+++ src/api/deleteChatUser.ts
@@ -1,18 +1,15 @@
-import variableAssignMent from "@utils/variableAssignment";
-import defaultRequest from "src/lib/axios/defaultRequest";
+import variableAssignMent from '@utils/variableAssignment';
+import defaultRequest from 'src/lib/axios/defaultRequest';
 
 interface ChatUserParams {
-  roomId: string;
-  targetId: string;
+    roomId: string;
+    targetId: string;
 }
 
-export const API_DELETE_CHAT_USER_KEY =
-  "/api/chat/{{roomId}}?targetId={{targetId}}";
+export const API_DELETE_CHAT_USER_KEY = '/api/chat/{{roomId}}?targetId={{targetId}}';
 
-const deletePartyDetail = async ({ roomId, targetId }: ChatUserParams) => {
-  await defaultRequest.delete(
-    variableAssignMent(API_DELETE_CHAT_USER_KEY, { roomId, targetId })
-  );
+const deletePartyUser = async ({ roomId, targetId }: ChatUserParams) => {
+    await defaultRequest.delete(variableAssignMent(API_DELETE_CHAT_USER_KEY, { roomId, targetId }));
 };
 
-export default deletePartyDetail;
+export default deletePartyUser;
